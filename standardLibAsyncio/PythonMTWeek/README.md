a93cae6f32c9befe478443f2169f554ebe102bf2
root <root@yx.utek.com>
2018-12-27 10:53:38 +0800

    增加PythonCaff的模块

---
+++ standardLibAsyncio/PythonMTWeek/README.md
@@ -0,0 +1,5 @@
+# 说明
+
+本模块为记录 Python3 Module of the Week 这本书的学习笔记。
+
+主要参考[PythonCaff](https://pythoncaff.com/docs/pymotw)翻译
---
+++ standardLibAsyncio/excample/21asyncio_condition.py
@@ -0,0 +1,42 @@
+"""
+条件变量Conditions
+"""
+import asyncio
+
+
+async def consumer(condition, n):
+    with await condition:
+        print('consumer {} is waiting'.format(n))
+        await condition.wait()
+        print('consumer {} triggered'.format(n))
+    print('ending consumer {}'.format(n))
+
+async def manipulate_condition(condition):
+    print('starting manipulate_condition')
+
+    await asyncio.sleep(0.1)
+
+    for i in range(1, 3):
+        with await condition:
+            print('notifying {} consumer'.format(i))
+            condition.notify(n=i)
+        await asyncio.sleep(2)
+
+    with await condition:
+        print('notify remaining consumer')
+        condition.notify_all()
+
+    print('ending manipulate_condition')
+
+async def main(loop):
+    condition = asyncio.Condition()
+
+    consumers = [consumer(condition, i) for i in range(5)]
+    loop.create_task(manipulate_condition(condition))
+    await asyncio.wait(consumers)
+
+event_loop = asyncio.get_event_loop()
+try:
+    result = event_loop.run_until_complete(main(event_loop))
+finally:
+    event_loop.close()
